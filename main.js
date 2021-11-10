cf437f8f32d6bea217c50e86e6779688054bb7c5
shloaky <77309360+shloaky@users.noreply.github.com>
2021-11-09 19:25:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+pose_net = ml5.poseNet(video,modelloaded);
+pose_net.on('pose',gotpose); 
+}
+
+function gotpose(results){
+if (results.length > 0){
+    console.log(results);
+    console.log("nose x = "+results[0].pose.nose.x);
+    console.log("nose y = "+results[0].pose.nose.y);
+    
+}
+}
+
+function modelloaded(){
+    console.log('posenet is initialized');
+}
+
+function draw(){
+image(video,0,0,300,300);
+}
+function takephoto(){
+    save('rednose.png');
+}
